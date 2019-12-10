a55a183213b1edd2bff95542580b0445e594b035
Shelby Hughes <shelbyhughes2019@gmail.com>
2019-12-10 15:20:33 -0600

    Picked third question and answered it
    exit

--- js/shelbys-three-problems.js
+++ js/shelbys-three-problems.js
@@ -1,5 +1,16 @@
 // Given a number and an object with min and max properties, return true if the number lies within the given range (inclusive).
 
+function isInRange(num, range) {
+    return (num >= range.min && num <= range.max);
+}
+
+console.log(isInRange(4, { min: 0, max: 5 })); // true
+console.log(isInRange(4, { min: 4, max: 5 }));// true
+console.log(isInRange(4, { min: 6, max: 10 })); // false
+console.log(isInRange(5, { min: 5, max: 5 })); // true
+
+// Source:https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
+
 // You hired three programmers and you (hopefully) pay them. Create a function that takes three numbers (the hourly wage of each programmer) and returns the difference between the highest-paid programmer and the lowest-paid.
 
 function programmers(one, two, three) {
@@ -13,3 +24,15 @@ console.log(programmers(33, 72, 74)); //41
 console.log(programmers(1, 5, 9)); //8
 
 //Source: https://www.w3schools.com/jsref/jsref_sort.asp
+
+// Create a function that takes an array of numbers and return the first and last elements as a new array.
+
+function firstLast(arr) {
+    return [arr[0], arr[arr.length - 1]];
+}
+
+console.log(firstLast([5, 10, 15, 20, 25])); // [5, 25]
+console.log(firstLast(["edabit", 13, null, false, true])); // ["edabit", true]
+console.log(firstLast([undefined, 4, "6", "hello", null])); // [undefined, null]
+
+//Source:https://stackoverflow.com/questions/39967872/return-a-new-array-with-the-first-and-last-element-of-the-passed-array
