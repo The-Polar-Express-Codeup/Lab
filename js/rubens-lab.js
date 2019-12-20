77eb960213b69d31500da088a8570444c7618227
Ruben Montanez <ruben.a.montanez@gmail.com>
2019-12-20 08:43:42 -0600

    added another problem

--- js/rubens-lab.js
+++ js/rubens-lab.js
@@ -13,3 +13,20 @@ assert(onlyPositiveEvens([2, -5, -6]), [2], "Exercise 77");
 assert(onlyPositiveEvens([3, 3, 4, 6]), [4, 6], "Exercise 77");
 assert(onlyPositiveEvens([2, 3, 4, -1, -5]), [2, 4], "Exercise 77");
 addToDone("Exercise 77 is correct.");
+
+// Write a function definition named hasEvens that takes in sequence of numbers and returns true if there are any even numbers in the sequence
+function hasEvens(arr) {
+    for(var i=0;i<arr.length;i++) {
+        if(arr[i] % 2 === 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+assert(hasEvens([1, 2, 3]), true, "Exercise 71");
+assert(hasEvens([2, 5, 6]), true, "Exercise 71");
+assert(hasEvens([3, 3, 3]), false, "Exercise 71");
+assert(hasEvens([]), false, "Exercise 71");
+addToDone("Exercise 71 is correct.");
