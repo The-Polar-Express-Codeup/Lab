a8a8f0b0bf08080e02c9cd316639460dfc0f1f3d
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-01-08 15:19:16 -0600

    Answered the third problem

--- js/shelbys-three-problems-part-3.js
+++ js/shelbys-three-problems-part-3.js
@@ -11,14 +11,39 @@ console.log(getStudentNames([
     {name: "John"}
 ])); // ➞ ["Steve", "Mike", "John"]
 
-// Create a function that takes a string and returns the word count. The string will be a sentence.
+// Create a function that takes a number as an argument and returns an array of numbers counting down from this number to zero.
 
-function countWords(str) {
-    return str.split(" ").length;
+function countdown(start) {
+    var array = [];
+    for(i = start; i>=0; i--){
+        array.push(i);
+    }
+    return array;
 }
 
+console.log(countdown(5)); // ➞ [5, 4, 3, 2, 1, 0]
+console.log(countdown(1)); // ➞ [1, 0]
+console.log(countdown(0)); // ➞ [0]
 
-console.log(countWords("This is a test")); // ➞ 4
-console.log(countWords("What an easy task, right")); // ➞ 5
-console.log(countWords("Just an example here move along")); // ➞ 6
+// Create a function to calculate the determinant of a 2 x 2 matrix. The determinant of the following matrix is: ad - bc:
 
+//[[a, b], [c, d]]
+
+function calcDeterminant(matrix) {
+    return (matrix[0][0]*matrix[1][1])-(matrix[0][1]*matrix[1][0]);
+}
+
+console.log(calcDeterminant([
+    [1, 2],
+    [3, 4]
+])); // ➞ -2
+
+console.log(calcDeterminant([
+    [5, 3],
+    [3, 1]
+])); // ➞ -4
+
+console.log(calcDeterminant([
+    [1, 1],
+    [1, 1]
+])); // ➞ 0
